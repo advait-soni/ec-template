06e55badfc261c501f768295c2ce7c607241edac
advait-soni <advaitsoni2020@gmail.com>
2025-05-07 20:40:29 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
+import { useRef } from "react"
+import { useEffect } from "react"
+import { useState } from "react"
 
 function App() {
+  let ref = useRef(0)
 
+  function handleClick() {
+    setValue(value + 1)
+  }
+  const [value, setValue] = useState(0)
+  const [num, setNum] = useState(0)
+
+  useEffect(() => {
+    setNum(num + 1)
+  }, [value])
 
   return (
     <>
-     <p className="">hello</p>
+      <p onClick={handleClick}> {num}</p>
     </>
   )
 }
