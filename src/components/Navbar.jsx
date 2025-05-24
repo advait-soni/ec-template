dc1209a10dedcdacd6d1c76317dc34cf75119e43
advait-soni <advaitsoni2020@gmail.com>
2025-05-24 17:31:56 +0530

    sticky navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,12 @@ const navbarBG = "bg-red-200 rounded-lg"
 
 function Navbar({ enhanceNav }) {
   return (
-    <nav className={` sticky top-0 flex justify-center mt-2 pb-8 ` + (enhanceNav ? "border-40" : "")}>
-      <ul className="flex space-x-40 px-12 py-3 ">
+    <nav className={" sticky top-0 flex justify-center mt-2 pb-8 "}>
+      <ul
+        className={`flex space-x-40 px-12 py-3 transition-[border-radius,background-color,border-color] duration-80 border ${
+          enhanceNav ? "border-black rounded bg-amber-100" : "border-white"
+        }`}
+      >
         {items.map((element, ind) => {
           return (
             <NavbarItem
